048c1dc9215ea07bc2a8f64895216ce6a20dcb3f
Rob Stewart <robstewart57@gmail.com>
2021-10-30 23:36:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HAFLANG site
+
+The website for the [HAFLANG project](https://gow.epsrc.ukri.org/NGBOViewGrant.aspx?GrantRef=EP/W009447/1)..
---
+++ site.hs
@@ -0,0 +1,91 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Monoid (mappend)
+import Hakyll
+import Text.Pandoc.Options
+
+--------------------------------------------------------------------------------
+config :: Configuration
+config =
+  defaultConfiguration
+    { destinationDirectory = "docs"
+    }
+
+main :: IO ()
+main = hakyllWith config $ do
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+
+  match "*.org" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
+
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Archives"
+              `mappend` defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Hardware Acceleration of Functional Languages"
+              `mappend` defaultContext
+
+      getResourceBody
+        -- pandocCompiler'
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateCompiler
+
+pandocCompiler' :: Compiler (Item String)
+pandocCompiler' = pandocCompilerWith pandocMathReaderOptions pandocMathWriterOptions
+
+pandocMathReaderOptions :: ReaderOptions
+pandocMathReaderOptions =
+  defaultHakyllReaderOptions
+    { readerExtensions = extensionsFromList [Ext_raw_html]
+    }
+
+pandocMathWriterOptions :: WriterOptions
+pandocMathWriterOptions =
+  defaultHakyllWriterOptions
+    { -- writerExtensions = extensions
+      writerHTMLMathMethod = MathJax ""
+    }
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+  dateField "date" "%B %e, %Y"
+    `mappend` defaultContext
